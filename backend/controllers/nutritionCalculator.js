cd624de66d9fd00026cfc4dd85ef765369393911
Charlotte <chbrit02@hs-esslingen.de>
2024-05-09 16:32:51 +0200

    Versuche

--- backend/controllers/nutritionCalculator.js
+++ backend/controllers/nutritionCalculator.js
@@ -1,11 +1,37 @@
+//import { getRouteParameter } from './tempController';
+
 const fetch = require('node-fetch');
+
+
+function nutritionCalculation(nutritionData, eatenQuantity) {
+  const { energy, fat, proteins, quantity } = nutritionData;
+
+  // Nutriments pro Gramm
+  const energyPerGram = energy / 100;
+  const fatPerGram = fat / 100;
+  const proteinsPerGram = proteins / 100;
+
+  // Nutriments je gegessene Quantity
+  const eatenEnergy = energyPerGram * eatenQuantity;
+  const eatenFat = fatPerGram * eatenQuantity;
+  const eatenProteins = proteinsPerGram * eatenQuantity;
+
+  return {
+      energy: eatenEnergy,
+      fat: eatenFat,
+      proteins: eatenProteins
+  };  
+}
+
+
+
 
 async function weightFetch(barcode, eatenQuantity) {
   try {
     const response = await fetch(`https://world.openfoodfacts.org/api/v3/product/${barcode}.json`); 
     const data = await response.json();
 
-    const energy = data.product.nutriments.energy || 0;
+    const energy = data.product.nutriments["energy-kcal_100g"] || 0;
     const fat = data.product.nutriments.fat || 0;
     const proteins = data.product.nutriments.proteins || 0;
 
@@ -29,27 +55,6 @@ async function weightFetch(barcode, eatenQuantity) {
       return null;
   }
 }
-
-
-function nutritionCalculation(nutritionData, eatenQuantity) {
-    const { energy, fat, proteins, quantity } = nutritionData;
-
-    // Nutriments pro Gramm
-    const energyPerGram = energy / quantity;
-    const fatPerGram = fat / quantity;
-    const proteinsPerGram = proteins / quantity;
-
-    // Nutriments je gegessene Quantity
-    const eatenEnergy = energyPerGram * eatenQuantity;
-    const eatenFat = fatPerGram * eatenQuantity;
-    const eatenProteins = proteinsPerGram * eatenQuantity;
-
-    return {
-        energy: eatenEnergy,
-        fat: eatenFat,
-        proteins: eatenProteins
-    };  
-  }
   
 
-module.exports = weightFetch;
+module.exports = weightFetch; 
